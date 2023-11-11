0c30d07e6c4e348e2c343909fe679ab3d2e3c3d3
Elbert  Ribeiro <elbertprofissional@gmail.com>
2023-11-11 20:49:51 -0300

    criando entidade

--- src/main/java/com/elbertribeiro/model/Aplicacao.java
+++ src/main/java/com/elbertribeiro/model/Aplicacao.java
@@ -1,14 +1,28 @@
 package com.elbertribeiro.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
-//@Entity
-//@Table(name = "TAB_APLICACAO", schema = "DEPLOY")
+@Entity
+@Table(name = "TAB_APLICACAO", schema = "DEPLOY")
+@Getter
+@Setter
 public class Aplicacao {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.AUTO)
-//    @Column(name = "APLICACAO_ID", columnDefinition = "NUMBER")
-//    private Long id;
+    @Id
+    @Column(name = "APLICACAO_ID", columnDefinition = "NUMBER")
+    private Long id;
 
+    @Column(name = "APLICACAO_DESCRICAO")
+    private String nomeAplicacao;
+
+    @Column(name = "APLICACAO_SERVIDOR_DEV")
+    private Long servidorDesenvolvimento;
+
+    @Column(name = "APLICACAO_SERVIDOR_HOM")
+    private Long servidorHomologacao;
+
+    @Column(name = "APLICACAO_SERVIDOR_PROD")
+    private Long servidorProducao;
 
 }
