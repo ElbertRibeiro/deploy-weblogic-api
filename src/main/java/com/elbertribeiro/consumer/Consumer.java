0ea67308a6fe9b8b5b8332af0d8a13db98a5203b
Elbert  Ribeiro <elbertprofissional@gmail.com>
2023-11-11 21:28:48 -0300

    criando filas de consumo

---
+++ src/main/java/com/elbertribeiro/consumer/Consumer.java
@@ -0,0 +1,55 @@
+package com.elbertribeiro.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Consumer {
+
+    private static final Logger logger = LoggerFactory.getLogger(Consumer.class);
+    private static final String TOPIC = "Tópico: {}";
+    private static final String KEY = "Key: {}";
+    private static final String HEADERS = "Headers: {}";
+    private static final String PARTION = "Partion: {}";
+    private static final String ORDER = "Order: {}";
+
+    @Value("${deploy.ambiente.dev}")
+    private String topicNameDev;
+
+    @Value("${deploy.ambiente.hom}")
+    private String topicNameHom;
+
+    @Value("${deploy.ambiente.prod}")
+    private String topicNameProd;
+
+    @KafkaListener(topics = "${deploy.ambiente.dev}", groupId = "${topic.name.group_id}")
+    public void consumeDev(ConsumerRecord<String, String> payload) {
+        logger.info(TOPIC, topicNameDev);
+        logger.info(KEY, payload.key());
+        logger.info(HEADERS, payload.headers());
+        logger.info(PARTION, payload.partition());
+        logger.info(ORDER, payload.value());
+    }
+
+    @KafkaListener(topics = "${deploy.ambiente.hom}", groupId = "${topic.name.group_id}")
+    public void consumeHom(ConsumerRecord<String, String> payload) {
+        logger.info(TOPIC, topicNameHom);
+        logger.info(KEY, payload.key());
+        logger.info(HEADERS, payload.headers());
+        logger.info(PARTION, payload.partition());
+        logger.info(ORDER, payload.value());
+    }
+
+    @KafkaListener(topics = "${deploy.ambiente.prod}", groupId = "${topic.name.group_id}")
+    public void consumeProd(ConsumerRecord<String, String> payload) {
+        logger.info(TOPIC, topicNameProd);
+        logger.info(KEY, payload.key());
+        logger.info(HEADERS, payload.headers());
+        logger.info(PARTION, payload.partition());
+        logger.info(ORDER, payload.value());
+    }
+}
