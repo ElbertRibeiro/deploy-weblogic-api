b1a2b3e6b085614e8bb9c6a43e91a4bb35012843
Elbert  Ribeiro <elbertprofissional@gmail.com>
2023-11-16 01:10:47 -0300

    adicionando feingclient ao projeto

--- src/main/java/com/elbertribeiro/DeployWeblogicApiApplication.java
+++ src/main/java/com/elbertribeiro/DeployWeblogicApiApplication.java
@@ -2,8 +2,10 @@ package com.elbertribeiro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class DeployWeblogicApiApplication {
 
     public static void main(String[] args) {
