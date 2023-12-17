814b1a5d8f1e5866314df29051da54a7fcfbe297
elbert.ribeiro <elbertprofissional@gmail.com>
2023-12-17 14:12:03 -0300

    adicionando record

--- src/main/java/com/elbertribeiro/controller/MyController.java
+++ src/main/java/com/elbertribeiro/controller/MyController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.togglz.core.manager.FeatureManager;
 
-import static com.elbertribeiro.enums.MyFeatures.DESCRIPTION_UPDATE;
+import static com.elbertribeiro.enums.Features.DESCRIPTION_UPDATE;
 
 @RestController
 public record MyController(FeatureManager featureManager) {
--- src/main/java/com/elbertribeiro/enums/Features.java
+++ src/main/java/com/elbertribeiro/enums/Features.java
@@ -3,7 +3,7 @@ package com.elbertribeiro.enums;
 import org.togglz.core.Feature;
 import org.togglz.core.annotation.Label;
 
-public enum MyFeatures implements Feature {
+public enum Features implements Feature {
 
     @Label("New description")
     DESCRIPTION_UPDATE
